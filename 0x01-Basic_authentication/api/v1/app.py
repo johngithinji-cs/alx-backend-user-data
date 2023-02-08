d6ab295c97376c3a34e6f0074bcbe2ffd1bc7457
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-08 08:45:06 +0300

    Update app.py
    Correcting documentation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,27 +23,25 @@ elif AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
 
 
-@app.errorhandler(401)
-def unahorised_error(error)-> str:
-    """
-    Unauthorised access
-    """
-    return jsonify({"error":"Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden_error(error)-> str:
-    """
-    Forbidden error
-    """
-    return jsonify({"error":"Forbidden"}), 403
-
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
@@ -54,12 +52,11 @@ def before_request() -> str:
     if auth is None:
         return
 
-
-     excluded_paths = ['/api/v1/status/',
+    excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
 
-     if not auth.require_auth(request.path, excluded_paths):
+    if not auth.require_auth(request.path, excluded_paths):
         return
 
     if auth.authorization_header(request) is None:
