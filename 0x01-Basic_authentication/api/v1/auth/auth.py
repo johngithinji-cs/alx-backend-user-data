a22df636037601b0ca9655a636dd7c7f7710e169
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-08 08:49:47 +0300

    Update auth.py
    Correcting documentation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-"""
-    Authentication module
+""" Module of Authentication
 """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
-    """API authentication class"""
+    """ Class to manage the API authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checking if an endpoint requires authentication"""
+        """ Method for validating if endpoint requires auth """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
 
         l_path = len(path)
         if l_path == 0:
             return True
-                slash_path = True if path[l_path - 1] == '/' else False
+
+        slash_path = True if path[l_path - 1] == '/' else False
 
         tmp_path = path
         if not slash_path:
@@ -44,7 +44,6 @@ class Auth:
 
         return request.headers.get("Authorization", None)
 
-
     def current_user(self, request=None) -> TypeVar('User'):
-         """ Validates current user """
-         return None
+        """ Validates current user """
+        return None
