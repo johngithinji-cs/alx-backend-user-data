5d2ba64057283bf280fde682c8870513cc3592c4
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-09 16:39:53 +0300

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """ Module of Session Authentication
 """
 from api.v1.auth.auth import Auth
@@ -7,27 +7,31 @@ import uuid
 
 
 class SessionAuth(Auth):
-    """ Session authenticating class"""
+    """Session Authentication Class"""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ creates a Session ID for a user_id """
-        if user_id is None or not instance(user_id, str):
+        """Creates a Session ID for a user_id"""
+
+        if user_id is None or not isinstance(user_id, str):
             return None
+
         session_id = str(uuid.uuid4())
+
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ returns a User ID based on a Session ID """
-        if session_id is None or not instance(session_id, str):
+        """Returns a User ID based on a Session ID"""
+
+        if session_id is None or not isinstance(session_id, str):
             return None
 
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
-        """ returns a User instance based on a cookie value """
+        """Returns a User instance based on a cookie value"""
 
         session_id = self.session_cookie(request)
 
@@ -39,15 +43,16 @@ class SessionAuth(Auth):
         return User.get(user_id)
 
     def destroy_session(self, request=None):
-        """  that deletes the user session / logout """
+        """Deletes de user session / logout"""
+
         if request is None:
             return False
-        
+
         session_id = self.session_cookie(request)
         if session_id is None:
             return False
 
-        user_id = user_id_for_session_id(session_id)
+        user_id = self.user_id_for_session_id(session_id)
 
         if not user_id:
             return False
