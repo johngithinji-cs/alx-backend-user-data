dd067723fbe131099bf6682bbfd3c0c4903f6743
johngithinji-cs <mathaijohn97@gmail.com>
2023-02-09 16:27:28 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,36 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie value """
+
+        session_id = self.session_cookie(request)
+
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """  that deletes the user session / logout """
+        if request is None:
+            return False
+        
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        try:
+            del self.user_id_by_session_id[session_id]
+        except Exception:
+            pass
+
+        return True
